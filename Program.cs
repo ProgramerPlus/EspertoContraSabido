8c2c0fc174fd19d3c1940c3c3849bbd8ab70ad60
ProgramerPlus <hipitelo@outlook.com>
2025-11-26 22:43:59 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear();
+Console.WriteLine("--- Esperto contra sabido ---");
+
+Console.WriteLine("Quantos alimentos serão distribuidos?");
+int alimento=Convert.ToInt32(Console.ReadLine()!);
+
+int n=0;
+int a =0;
+double s=0;
+int soma=0;
+while(n<alimento){
+
+    n=n+1;
+    Console.Write($"{n} para você. ");
+
+while(a<n){
+   a=a+1;
+    Console.Write($"{a} ");
+    
+}
+
+if(){
+
+  
+}
+
+ Console.Write($" para mim.\n");
+
+}
+
+
+Console.WriteLine($"Pica-pau recebeu {n} alimento(s).");
+Console.WriteLine($"Raposinha recebeu {soma} alimento(s).");
