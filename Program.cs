f223bdb55d5e49326e97caf3aed12aa9e2201a92
ProgramerPlus <hipitelo@outlook.com>
2025-11-28 22:29:15 -0300

    Exercício em andamento

--- Program.cs
+++ Program.cs
@@ -6,28 +6,37 @@ int alimento=Convert.ToInt32(Console.ReadLine()!);
 
 int n=0;
 int a =0;
-double s=0;
+int s=0;
 int soma=0;
+int x=0;
+int l=0;
+
 while(n<alimento){
 
     n=n+1;
     Console.Write($"{n} para você. ");
 
+a=0;
 while(a<n){
    a=a+1;
-    Console.Write($"{a} ");
-    
+   Console.Write($"{a} ");
 }
 
-if(){
+s=a+s;
 
-  
-}
+soma=n+s;   
+
+x = alimento-soma; 
+
+
+
+
 
  Console.Write($" para mim.\n");
+
 
 }
 
 
 Console.WriteLine($"Pica-pau recebeu {n} alimento(s).");
-Console.WriteLine($"Raposinha recebeu {soma} alimento(s).");
+Console.WriteLine($"Raposinha recebeu {s} alimento(s).");
