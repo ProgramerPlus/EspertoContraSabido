30b1840c1efc187742a131e99c9005856df7a4f2
ProgramerPlus <hipitelo@outlook.com>
2025-12-01 20:21:54 -0300

    Exercício em andamento

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿using System.ComponentModel;
+
+Console.Clear();
 Console.WriteLine("--- Esperto contra sabido ---");
 
 Console.WriteLine("Quantos alimentos serão distribuidos?");
@@ -10,16 +12,22 @@ int s=0;
 int soma=0;
 int x=0;
 int l=0;
+int y=0;
+int t=0;
 
 while(n<alimento){
 
     n=n+1;
     Console.Write($"{n} para você. ");
+y=n;
+if(l==1 && x==0 || alimento==n){n=alimento;break;}
 
 a=0;
 while(a<n){
    a=a+1;
+   if(x==a || x==0 && l==1){n=alimento;break;}
    Console.Write($"{a} ");
+   
 }
 
 s=a+s;
@@ -30,13 +38,15 @@ x = alimento-soma;
 
 
 
+if(x<=n){l=1;}
 
 
  Console.Write($" para mim.\n");
 
+if(l==1 && x==0){n=alimento;break;}
 
 }
 
 
-Console.WriteLine($"Pica-pau recebeu {n} alimento(s).");
+Console.WriteLine($"Pica-pau recebeu {y} alimento(s).");
 Console.WriteLine($"Raposinha recebeu {s} alimento(s).");
